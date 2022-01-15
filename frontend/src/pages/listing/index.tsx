08744e9f04de9bb33c0b79febce8e5dd2be2f5e3
Joao Vitor de Morais Soares <joaovitormoraissoares@gmail.com>
2022-01-15 20:12:56 -0300

    Security config

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -11,12 +11,6 @@ export default function Listing(){
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
                 </div>
             </div>  
         </>
